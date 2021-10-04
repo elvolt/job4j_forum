462e9bb830cf4b1132dcc68e4a9515823484d107
elvolt <sidelevcs@gmail.com>
2021-10-04 21:30:03 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,23 +1,34 @@
 package ru.job4j.forum.control;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
 
+import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 @Sql("/data.sql")
 public class PostControlTest {
+
+    @MockBean
+    private PostService service;
 
     @Autowired
     private MockMvc mockMvc;
@@ -48,5 +59,33 @@ public class PostControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("post"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenCreateNewPostThenReturnPostName() throws Exception {
+        this.mockMvc.perform(post("/save")
+                .param("name", "Post")
+                .param("desc", "Post description"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(service).savePost(argument.capture(), eq("user"));
+        Assertions.assertEquals("Post", argument.getValue().getName());
+    }
+
+    @Test
+    @WithMockUser
+    public void serviceShouldUpdateTopic() throws Exception {
+        this.mockMvc.perform(post("/save")
+                .param("id", "1")
+                .param("name", "Post")
+                .param("desc", "Post description"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(service).savePost(argument.capture(), eq("user"));
+        Assertions.assertEquals(1, argument.getValue().getId());
+        Assertions.assertEquals("Post", argument.getValue().getName());
     }
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,10 +1,12 @@
-INSERT INTO authorities (authority)
-VALUES ('ROLE_USER');
-INSERT INTO authorities (authority)
-VALUES ('ROLE_ADMIN');
+DELETE FROM posts;
+DELETE FROM users;
+DELETE FROM authorities;
 
-INSERT INTO users (username, enabled, password, authority_id)
-VALUES ('root', true, '$2a$10$1jr6MBlUV879JWTxo3jbUuXn80gQTn15/S2ktCEgO.X5oLusJubTK', 1);
+INSERT INTO authorities (id, authority)
+VALUES (1, 'ROLE_USER');
 
-INSERT INTO posts (name, description, created, author_id)
-VALUES ('post1', 'description1', now(), 1);
+INSERT INTO users (id, username, enabled, password, authority_id)
+VALUES (1, 'root', true, '$2a$10$1jr6MBlUV879JWTxo3jbUuXn80gQTn15/S2ktCEgO.X5oLusJubTK', 1);
+
+INSERT INTO posts (id, name, description, created, author_id)
+VALUES (1, 'post1', 'description1', now(), 1);
